795c96e1fcf68c650ecf0e09b52af777fc1fd5c2
ivanForever <ivanGGfake@gmail.com>
2023-04-09 21:53:47 -0500

    Agregando archivos

---
+++ Socket1/src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Intro with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+
+        System.out.println("Hoy en dia en muchas empresas se han implementado un sistema en donde un servidor es clave para guardar la informacion y tambien para poder" +
+                "tener una interaccion entre los trabajadores y clientes con la empresa, ya que dicha implementacion ha mejorado con el tiempo y transformado el " +
+                "metodo de trabajo de las empresas, dando mas eficiencia y facilitando algunas actividades que solian hacerse de forma manual.");
+
+
+    }
+}
